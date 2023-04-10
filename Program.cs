58d07ef570f38c57ca52906fa24cc4cb6e6a2169
LotusPenguin <szybkipingwin@gmail.com>
2023-04-10 17:30:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT_Lab6_Server
+{
+    public class Program
+    {
+        UdpClient server;
+        List<IPEndPoint> clients;
+
+        static void Main()
+        {
+            var program = new Program
+            {
+                server = new UdpClient(13131),
+                clients = new List<IPEndPoint>()
+            };
+
+            var listenTask = Task.Factory.StartNew(() => program.Listen());
+            var collectTask = Task.Factory.StartNew(() => program.Collect());
+            var sendTask = Task.Factory.StartNew(() => program.Send());
+
+            Console.WriteLine("Server available at: " + ((IPEndPoint)program.server.Client.LocalEndPoint).Port);
+
+            Task.WaitAll(listenTask, collectTask, sendTask);
+        }
+
+        private void Listen()
+        {
+            while (true)
+            {
+
+            }
+        }
+
+        private void Collect()
+        {
+
+        }
+
+        private void Send()
+        {
+
+        }
+    }
+}
